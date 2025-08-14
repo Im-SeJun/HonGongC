9e95a75bfb725b7afb0afbbfaf9eda0c05b511ee
Im-SeJun <sejun4984@gmail.com>
2025-08-14 10:12:18 +0900

    25.08.14. 업로드

--- 14단원/예제14-7.c
+++ 14단원/예제14-7.c
@@ -3,19 +3,19 @@
 int main(void)
 {
 	int ary1[4] = { 1, 2, 3, 4 };
-	int ary2[4] = { 11, 12, 13, 14 };
-	int ary3[4] = { 21, 22, 23, 24 };
+	int ary2[5] = { 11, 12, 13, 14 };
+	int ary3[5] = { 21, 22, 23, 24, 25 };
 	int* pary[3] = { ary1, ary2, ary3 };
 	int i, j;
 
 	for (i = 0; i < 3; i++)
 	{
-		for (j = 0; j < 4; j++)
+		for (j = 0; j < 5; j++)
 		{
 			printf("%5d", pary[i][j]);
 		}
 		printf("\n");
 	}
-
+	printf("%5d", pary[2][2]);
 	return 0;
 }
